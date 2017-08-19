a3ccfbb9855597304e6c49035353bdffa47d4336
nikkiiworld <30777505+nikkiiworld@users.noreply.github.com>
2017-08-19 19:13:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,6 @@
         if( have_posts() ) :
             while ( have_posts() ) : the_post();
                     get_template_part( 'template-parts/content/content', get_post_format() ? : 'post' );
-
-                // If comments are open or we have at least one comment, load up the comment template.
-                if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif;
-
                 // End the loop.
             endwhile;
         ?>
